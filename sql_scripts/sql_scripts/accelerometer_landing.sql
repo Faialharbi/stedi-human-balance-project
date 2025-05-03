e1fbd1ebd34892812bf70677690e1e6c1b4587ba
Faialharbi <151734998+Faialharbi@users.noreply.github.com>
2025-05-03 18:57:31 +0300

    Delete sql_scripts/sql_scripts directory

--- sql_scripts/sql_scripts/accelerometer_landing.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS stedi_db.accelerometer_landing (
-  user STRING,
-  timestamp BIGINT,
-  x FLOAT,
-  y FLOAT,
-  z FLOAT
-)
-STORED AS PARQUET
-LOCATION 's3://stedi-fai/landing/accelerometer/';
--- sql_scripts/sql_scripts/sql_scripts/step_trainer_landing.sql
+++
@@ -1,7 +0,0 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS stedi_db.step_trainer_landing (
-  sensorReadingTime BIGINT,
-  serialNumber STRING,
-  distanceFromObject INT
-)
-STORED AS PARQUET
-LOCATION 's3://stedi-fai/landing/step_trainer/';
